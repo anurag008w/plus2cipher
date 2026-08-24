8e78ce553d3e112943a6ff6b11156a1e74b74408
anurag008w <202967512+anurag008w@users.noreply.github.com>
2026-08-24 09:58:22 +0530

    fix: force pure python charset_normalizer to fix pip strict wheel tag error

---
+++ p4a-recipes/charset_normalizer/__init__.py
@@ -0,0 +1,14 @@
+from pythonforandroid.recipe import PythonRecipe
+
+class CharsetNormalizerRecipe(PythonRecipe):
+    version = '3.5.1'
+    url = 'https://pypi.python.org/packages/source/c/charset-normalizer/charset-normalizer-{version}.tar.gz'
+    depends = ['setuptools']
+    
+    def get_recipe_env(self, arch=None, with_flags_in_cc=True):
+        env = super().get_recipe_env(arch, with_flags_in_cc)
+        # Force pure python build by disabling mypyc compilation
+        env['CHARSET_NORMALIZER_USE_MYPYC'] = '0'
+        return env
+
+recipe = CharsetNormalizerRecipe()
